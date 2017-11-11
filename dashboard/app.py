c5d1dbc845eca5bf2e171a8f9291fdeb727704ce
Sachin Jain <sach2000@gmail.com>
2017-11-11 16:14:47 -0500

    'login'

--- dashboard/app.py
+++ dashboard/app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/", methods = ["POST"])
+def my_form_post():
+	username = request.form['username']
+	password = request.form['password']
+	if username == "admin" and password == "admin":
+		return render_template("dashboard.html")
+
 if __name__ == "__main__":
 	app.run(debug = True)
 	
