9993d45003543f0b34e4fb3da22da3117c17ba2c
Sachin Jain <sach2000@gmail.com>
2017-11-12 03:10:53 -0500

    'loginfixed'

--- dashboard/app.py
+++ dashboard/app.py
@@ -9,8 +9,10 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-@app.route("/dashboard", methods = ["POST"])
+@app.route("/dashboard", methods = ["POST", "GET"])
 def my_form_post():
+	if request.method == "GET":
+		return render_template("dashboard.html")
 	username = request.form['username']
 	password = request.form['password']
 	data = {
@@ -20,9 +22,10 @@ def my_form_post():
 	r = requests.post('http://10.199.25.174:5000/api/v1/plogin', data = data)
 	print(r.text, file=sys.stderr)
 
-	if r.text == "True":
+	if r.text != "Fail":
 		resp = make_response(render_template("dashboard.html"))
 		resp.set_cookie('username', username)
+		resp.set_cookie('session_id', r.text)
 		return resp
 	return render_template("index.html")
 
@@ -42,6 +45,17 @@ def my_form_register():
 	}
 	r = requests.post('http://10.199.25.174:5000/api/v1/pregister', data = data)
 	return render_template("dashboard.html")
+
+@app.route("/user")
+def user():
+	username = request.cookies.get('username')
+	session_id = requests.cookies.get('session_id')
+	data = {
+		'username': username,
+		'session_id': session_id
+	}
+	r = requests.post()
+	return render_template("user.html", data = data)
 
 @app.route("/logout")
 def logout():
